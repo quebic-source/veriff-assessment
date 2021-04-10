dabf919e55ab77b7d406399fb71962e2ea93bcde
Tharanga_Thennakoon <tharanganilupul@gmail.com>
2021-04-10 16:38:56 +0530

    .

--- veriff/__main__.py
+++ veriff/__main__.py
@@ -1,9 +1,14 @@
+import time
+
 from veriff.helper.logger_util import get_logger
 from veriff import pipeline
 
 logger = get_logger(__name__)
 
 if __name__ == "__main__":
-    logger.info("Application starting")
+    start_time = time.time()
+    logger.info("application starting")
     status = pipeline.execute()
-    logger.info("Application stopped status:%s", "OK" if status else "FAILED")
+    logger.info("application stopped status:%s. took %r seconds"
+                , "OK" if status else "FAILED"
+                , (time.time() - start_time))
--- veriff/handler/execution_handler.py
+++ veriff/handler/execution_handler.py
@@ -14,7 +14,13 @@ def face_encodings(image_path):
     start_time = time.time()
     logger.info("face_encodings start %r", image_path)
     image_data = face_recognition.load_image_file(image_path)
-    face_vector = face_recognition.face_encodings(image_data)[0]
+
+    face_vectors = face_recognition.face_encodings(image_data)
+    if not face_vectors:
+        logger.error("face_encodings failed. not found vector. took %r seconds", (time.time() - start_time))
+        return None
+
+    face_vector = face_vectors[0]
     logger.info("face_encodings completed. took %r seconds", (time.time() - start_time))
     return face_vector
 
@@ -34,10 +40,20 @@ class FaceVectorCalculatorHandler(AbstractHandler):
 
         face_images_data_list = context.data_set
 
-        cpu_count = mp.cpu_count()
-        pool_size = min(cpu_count, len(face_images_data_list))
-        with Pool(pool_size) as p:
-            face_vectors = p.map(face_encodings, face_images_data_list)
-            context.result = len(face_vectors)
-            logger.info("face-vector calculator completed. took %r seconds", (time.time() - start_time))
-            self.next.handle(context)
+        # cpu_count = mp.cpu_count()
+        # pool_size = min(cpu_count, len(face_images_data_list))
+        # with Pool(pool_size) as p:
+        #     face_vectors = p.map(face_encodings, face_images_data_list)
+        #     context.result = len(face_vectors)
+        #     logger.info("face-vector calculator completed. took %r seconds", (time.time() - start_time))
+        #     self.next.handle(context)
+
+        face_vectors = []
+        for face_image_path in face_images_data_list:
+            vector = face_encodings(face_image_path)
+            if vector is not None:
+                face_vectors.append(vector)
+
+        context.result = len(face_vectors)
+        logger.info("face-vector calculator completed. took %r seconds", (time.time() - start_time))
+        self.next.handle(context)
